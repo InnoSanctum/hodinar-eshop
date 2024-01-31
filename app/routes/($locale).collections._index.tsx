ffd8192c0228d12a5872b19338e700363a831037
Vojtech Suchanek <suchavo3@cvut.cz>
2024-01-31 17:55:42 +0100

    filters

--- app/routes/($locale).collections._index.tsx
+++ app/routes/($locale).collections._index.tsx
@@ -9,8 +9,9 @@ import VojtikLink from '~/components/custom/VojtikLink';
 import Loading from '~/components/Loading';
 import {useLanguage} from '~/utils';
 import Product from '~/components/Product';
-import {useState} from 'react';
+import {ReactElement, useEffect, useState} from 'react';
 import {motion, AnimatePresence} from 'framer-motion';
+import arrow from '../../public/assets/svgs/arrow.svg';
 
 export async function loader({context, request}: LoaderFunctionArgs) {
   const paginationVariables = getPaginationVariables(request, {
@@ -21,8 +22,11 @@ export async function loader({context, request}: LoaderFunctionArgs) {
   const {collections} = await context.storefront.query(COLLECTIONS_QUERY, {
     variables: paginationVariables,
   });
-
-  return json({collections, products});
+  const url = new URL(request.url);
+  const data = url.searchParams.get('filters');
+  console.log(url, data);
+  const filtersUrl: Filter | null = data && JSON.parse(data);
+  return json({collections, products, filtersUrl, href: url.href});
 }
 
 interface Filter {
@@ -33,14 +37,33 @@ interface Filter {
 }
 
 export default function Collections() {
-  const {collections, products} = useLoaderData<typeof loader>();
-
-  const [filters, setFilters] = useState<Filter>({
-    price: {from: undefined, to: undefined},
-    availability: undefined,
-    brand: undefined,
-    style: undefined,
-  });
+  const {collections, products, filtersUrl, href} =
+    useLoaderData<typeof loader>();
+  const [filters, setFilters] = useState<Filter>(
+    /* {
+      price: {from: undefined, to: undefined},
+      availability: undefined,
+      brand: undefined,
+      style: undefined,
+    } */ (filtersUrl as Filter) || {
+      price: {from: undefined, to: undefined},
+      availability: undefined,
+      brand: undefined,
+      style: undefined,
+    },
+  );
+  function createUrl() {
+    const url = new URL(location.href);
+    return `${url.origin}${url.pathname}?filters=${encodeURIComponent(
+      JSON.stringify(filters),
+    )}`;
+  }
+  useEffect(() => {
+    console.log(href);
+    const newUrl = createUrl();
+    console.log(newUrl);
+    if (href !== newUrl) window.history.pushState({id: 1}, 'xd', newUrl);
+  }, [filters]);
   const language = useLanguage();
   return (
     <div className="collections">
@@ -134,6 +157,32 @@ function parseBrand(string: string): string {
   return string.split('-')[1];
 }
 
+function FilterSection({
+  name,
+  children,
+}: {
+  name: string;
+  children: ReactElement;
+}) {
+  const [isOpen, setOpenState] = useState(false);
+  return (
+    <div className="p-4 border-t-2 border-secondary flex flex-wrap gap-4 lg:flex-col lg:min-w-56">
+      <span
+        onClick={() => setOpenState(!isOpen)}
+        className="flex justify-between cursor-pointer w-full"
+      >
+        <h4>{name}</h4>
+        <img
+          style={{transform: `rotate(${isOpen ? -90 : 90}deg)`}}
+          className="h-6 transition-all"
+          src={arrow}
+        />
+      </span>
+      {isOpen&&<>{children}</>}
+    </div>
+  );
+}
+
 function Filters({
   setFilters,
   products,
@@ -206,106 +255,110 @@ function Filters({
       <div className=" lg:sticky lg:top-20">
         <div className="filter__height overflow-y-scroll">
           {language.filters.filter}:
-          <div className="p-4 border-t-2 border-secondary flex flex-wrap gap-4 lg:flex-col lg:min-w-56">
-            <h4>{language.filters.availability}</h4>
-            <span>
+          <FilterSection name={language.filters.availability}>
+            <>
+              <span>
+                <input
+                  onChange={() => setAvailability(true)}
+                  id="dostupné"
+                  type="checkbox"
+                  checked={
+                    filters.availability === undefined
+                      ? false
+                      : filters.availability
+                  }
+                  className="w-4 h-4 rounded focus:ring-blue-500focus:ring-2border-gray-600"
+                />
+                <label htmlFor="dostupné" className="ms-2 font-medium">
+                  {language.filters.avaible}
+                </label>
+              </span>
+              <span>
+                <input
+                  onChange={() => setAvailability(false)}
+                  id="prodané"
+                  type="checkbox"
+                  checked={
+                    filters.availability === undefined
+                      ? false
+                      : !filters.availability
+                  }
+                  className="w-4 h-4 rounded focus:ring-blue-500focus:ring-2border-gray-600"
+                />
+                <label htmlFor="prodané" className="ms-2 font-medium">
+                  {language.filters.sold}
+                </label>
+              </span>
+            </>
+          </FilterSection>
+          <FilterSection name={language.filters.style}>
+            <>
+              {styles.map((style, index) => {
+                return (
+                  <span key={index}>
+                    <input
+                      onChange={() => setStyle(style.handle)}
+                      id={style.handle}
+                      type="checkbox"
+                      checked={filters.style?.includes(style.handle)}
+                      className="w-4 h-4 rounded focus:ring-blue-500focus:ring-2border-gray-600"
+                    />
+                    <label htmlFor={style.handle} className="ms-2 font-medium">
+                      {style.title}
+                    </label>
+                  </span>
+                );
+              })}
+            </>
+          </FilterSection>
+          <FilterSection
+            name={`${language.filters.price} (${products[0].priceRange.minVariantPrice.currencyCode})`}
+          >
+            <>
               <input
-                onChange={() => setAvailability(true)}
-                id="dostupné"
-                type="checkbox"
-                checked={
-                  filters.availability === undefined
-                    ? false
-                    : filters.availability
-                }
-                className="w-4 h-4 rounded focus:ring-blue-500focus:ring-2border-gray-600"
+                name="q"
+                placeholder={language.filters.from}
+                type="number"
+                className="text-primary bg-secondary"
+                onChange={(e) => {
+                  console.log(e.target.value);
+                  filters.price.from = parseInt(e.target.value) || undefined;
+                  setFilters({...filters});
+                }}
               />
-              <label htmlFor="dostupné" className="ms-2 font-medium">
-                {language.filters.avaible}
-              </label>
-            </span>
-            <span>
               <input
-                onChange={() => setAvailability(false)}
-                id="prodané"
-                type="checkbox"
-                checked={
-                  filters.availability === undefined
-                    ? false
-                    : !filters.availability
-                }
-                className="w-4 h-4 rounded focus:ring-blue-500focus:ring-2border-gray-600"
+                name="q"
+                placeholder={language.filters.to}
+                type="number"
+                className="text-primary bg-secondary"
+                onChange={(e) => {
+                  console.log(e.target.value);
+                  filters.price.to = parseInt(e.target.value) || undefined;
+                  setFilters({...filters});
+                }}
               />
-              <label htmlFor="prodané" className="ms-2 font-medium">
-                {language.filters.sold}
-              </label>
-            </span>
-          </div>
-          <div className="p-4 border-t-2 border-secondary flex flex-wrap gap-4 lg:flex-col">
-            <h4>{language.filters.style}</h4>
-            {styles.map((style, index) => {
-              return (
-                <span key={index}>
-                  <input
-                    onChange={() => setStyle(style.handle)}
-                    id={style.handle}
-                    type="checkbox"
-                    checked={filters.style?.includes(style.handle)}
-                    className="w-4 h-4 rounded focus:ring-blue-500focus:ring-2border-gray-600"
-                  />
-                  <label htmlFor={style.handle} className="ms-2 font-medium">
-                    {style.title}
-                  </label>
-                </span>
-              );
-            })}
-          </div>
-          <div className="p-4 border-t-2 border-secondary flex flex-wrap gap-4 lg:flex-col">
-            <h4>
-              {language.filters.price} ({products[0].priceRange.minVariantPrice.currencyCode})
-            </h4>
-            <input
-              name="q"
-              placeholder={language.filters.from}
-              type="number"
-              className="text-primary bg-secondary"
-              onChange={(e) => {
-                console.log(e.target.value);
-                filters.price.from = parseInt(e.target.value) || undefined;
-                setFilters({...filters});
-              }}
-            />
-            <input
-              name="q"
-              placeholder={language.filters.to}
-              type="number"
-              className="text-primary bg-secondary"
-              onChange={(e) => {
-                console.log(e.target.value);
-                filters.price.to = parseInt(e.target.value) || undefined;
-                setFilters({...filters});
-              }}
-            />
-          </div>
-          <div className="p-4 border-t-2 border-secondary flex flex-wrap gap-4 lg:flex-col">
-            <h4>{language.filters.clockwork}</h4>
-            {brands.map((brand, index) => {
-              return (
-                <span key={index}>
-                  <input
-                    onChange={() => setBrand(brand.handle)}
-                    id={brand.handle}
-                    type="checkbox"
-                    checked={filters.brand?.includes(brand.handle)}
-                    className="w-4 h-4 rounded focus:ring-blue-500focus:ring-2border-gray-600"
-                  />
-                  <label htmlFor={brand.handle} className="ms-2 font-medium">
-                    {parseBrand(brand.title)}
-                  </label>
-                </span>
-              );
-            })}
-          </div>
+            </>
+          </FilterSection>
+          <FilterSection name={language.filters.clockwork}>
+            <>
+              {brands.map((brand, index) => {
+                return (
+                  <span key={index}>
+                    <input
+                      onChange={() => setBrand(brand.handle)}
+                      id={brand.handle}
+                      type="checkbox"
+                      checked={filters.brand?.includes(brand.handle)}
+                      className="w-4 h-4 rounded focus:ring-blue-500focus:ring-2border-gray-600"
+                    />
+                    <label htmlFor={brand.handle} className="ms-2 font-medium">
+                      {parseBrand(brand.title)}
+                    </label>
+                  </span>
+                );
+              })}
+            </>
+          </FilterSection>
         </div>
       </div>
     </div>
